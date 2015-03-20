aba1715abada53c85ba9be1af166ed626e778096
Elvir Kuric <ekuric@redhat.com>
2015-03-20 11:43:05 +0100

    added build-kubernetes.sh - script to build kubernetes packages

---
+++ kubernetes/build-kubernetes.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+printf "%s\n" "Script to download kubernets source and build binary files"
+printf "%s\n" "Supposed to run on RHEL based distributions"
+
+# install software
+
+yum install -y golang glibc-static device-mapper-devel btrfs-progs btrfs-progs-devel sqlite-devel docker git
+
+systemctl start docker
+systemctl enable docker
+
+printf "%s\n" "Clonning kubernetes code and starting build"
+
+git clone https://github.com/GoogleCloudPlatform/kubernetes
+cd kubernetes/build
+sed -i 's/KUBE_SKIP_CONFIRMATIONS\:\-n/KUBE_SKIP_CONFIRMATIONS\:\-y/' common.sh
+./release.sh
+
+cd ~/kubernetes/_output/dockerized/bin/linux/amd64
+
+printf "%s\n" "Binaries are build and can be found in ~/kubernetes/_output/dockerized/bin/linux/amd64"
+
+ls -l
+
+exit 0
+
+
+
