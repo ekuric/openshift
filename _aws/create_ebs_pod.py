c1ca8ff0e5b6767937412f0f8ce64f269435b83c
Elvir Kuric <elvirkuric@gmail.com>
2016-03-03 13:29:58 +0100

    updated crete_ebs_pod.py
    Signed-off-by: Elvir Kuric <elvirkuric@gmail.com>

--- _aws/create_ebs_pod.py
+++ _aws/create_ebs_pod.py
@@ -1,37 +1,34 @@
 # program to create Amazon EC2 volumes
+__author__ = "elko"
 
 import argparse
 import boto3
 import subprocess
 import os
 import json
-import sys
 
 def main():
 
-    # parse arguments
-    parser = argparse.ArgumentParser(description="Script to create Amazon EBS volumes")
-    parser.add_argument("--vnumber", help="Number of EBS volume to create", default=1, type=int)
+    # optional arguments
+    parser = argparse.ArgumentParser(description="Script to create OSE pods and attach one EBS volume per pod as persistent storage")
     parser.add_argument("--volumesize", help="size of EBS voluems - in GB ", default=5, type=int)
-    parser.add_argument("--vtype", help="volume type", default="gp2")
+    parser.add_argument("--vtype", help="EBS volume type, default is gp2", default="gp2")
     parser.add_argument("--region", help="Amazon region where to connect", default="us-west-2b")
     parser.add_argument("--image", help="docker image to use", default="r7perffio")
-    parser.add_argument("--pnumber", help="number of pods", default=1, type=int)
-    parser.add_argument("--tagprefix", help="tag prefix - something to differ tags", default="openshift-testing")
-    parser.add_argument("--mountpoint", help="mount point inside pod where volume will be mounted", default="/mnt/persistentvolume")
-    parser.add_argument("--snumber", help="start value from which pods will be created, default is 0, - pod0, pod1, ... pnumber will be created", type=int, default=0)
+    parser.add_argument("--tagprefix", help="tag prefix for EBS volumes, default tag is openshift-testing-EBS_volume_id", default="openshift-testing")
+    parser.add_argument("--mountpoint", help="mount point inside pod where EBS volume will be mounted, default is /mnt/persistentvolume", default="/mnt/persistentvolume")
+    parser.add_argument("--minpod", help="minimum number of pods to create - default is 1 - so minimum one pod will be created", type=int, default=1)
+    parser.add_argument("--action", help="what to do  - either to create pods or delete pods", default="create")
 
     # required arguments
-    parser.add_argument("--enumber", help="end value for number of pods", type=int, required=True)
-    parser.add_argument("--pvfile", help="persistent volume definition json file", required=True)
-    parser.add_argument("--pvcfile", help="persistent volume claim definition json file", required=True)
-    parser.add_argument("--podfile", help="pod definition json file", required=True)
+    parser.add_argument("--maxpod", help="maximum number of pods to create - required parameter", type=int, required=True)
+    parser.add_argument("--pvfile", help="persistent volume definition json file - required parameter", required=True)
+    parser.add_argument("--pvcfile", help="persistent volume claim definition json file - required parameter", required=True)
+    parser.add_argument("--podfile", help="pod definition json file - required parameter", required=True)
 
 
     args = parser.parse_args()
 
-    vnumber  =  args.vnumber
-    pnumber = args.pnumber
     volumesize = args.volumesize
     vtype = args.vtype
     region = args.region
@@ -41,18 +38,25 @@ def main():
     pvfile = args.pvfile
     pvcfile = args.pvcfile
     podfile = args.podfile
-    snumber = args.snumber
-    enumber = args.enumber
+    minpod = args.minpod
+    maxpod = args.maxpod
+    action = args.action
 
     session = boto3.Session(region_name=region)
     ec2 = boto3.resource("ec2")
 
     def create_ebs_pod():
+        """
+        create_ebs_pod() function will do
+        - create EBS volume
+        - tag EBS volume
+        - create PV/PVC
+        - crete pod and attach already created EBS volume to POD as persistent storage
+        """
+        global minpod
+        minpod = args.minpod
 
-        global snumber
-        snumber = args.snumber
-
-        while snumber < enumber:
+        while minpod < maxpod:
 
             volume = ec2.create_volume(VolumeType=vtype,
                                        AvailabilityZone=region,
@@ -73,7 +77,7 @@ def main():
                     print ("Cannot open file persistent volume file, is it specified?")
 
             pvdoc = json.load(pvhandler)
-            pvvanila = { "metadata": {"name": "pvolume" + str(snumber) },
+            pvvanila = { "metadata": {"name": "pvolume" + str(minpod) },
                        'spec': {'capacity': {'storage': volumesize },
                         'persistentVolumeReclaimPolicy': 'Recycle', 'accessModes': ['ReadWriteOnce'],
                         'awsElasticBlockStore': {'volumeID': volume.id , 'fsType': 'ext4'}}}
@@ -92,7 +96,7 @@ def main():
                 print ("Cannot open persistent volume claim file, is it specified?")
 
             pvcdoc = json.load(pvchandler)
-            pvcvanila = {'metadata': {"name":"pvclaim" + str(snumber)},
+            pvcvanila = {'metadata': {"name":"pvclaim" + str(minpod)},
                           'spec': {'accessModes': ['ReadWriteOnce'], 'resources': {'requests': {'storage': volumesize }}}}
 
             pvcdoc.update(pvcvanila)
@@ -107,24 +111,24 @@ def main():
                 print ("Cannot open pod file, is it specified? Check options")
 
             poddoc = json.load(podhandler)
-            podvanila = {'metadata': {'name': "pod" + str(snumber)},
+            podvanila = {'metadata': {'name': "pod" + str(minpod)},
                          'spec': {'containers': [{'image': image,
                                                   'imagePullPolicy': 'IfNotPresent',
-                                                  'name': "pod" + str(snumber),
-                                                  'volumeMounts': [{'name': 'pvolume' + str(snumber),
+                                                  'name': "pod" + str(minpod),
+                                                  'volumeMounts': [{'name': 'pvolume' + str(minpod),
                                                   'mountPath': mountpoint }]}],
-                                                  'volumes': [{'persistentVolumeClaim': {'claimName': "pvclaim" + str(snumber)}, 'name': "pvolume" + str(snumber)}]}}
+                                                  'volumes': [{'persistentVolumeClaim': {'claimName': "pvclaim" + str(minpod)}, 'name': "pvolume" + str(minpod)}]}}
             poddoc.update(podvanila)
             json.dump(poddoc, open("podfile.json", "w+"))
             subprocess.call(["oc", "create", "-f", "podfile.json"])
             os.remove("podfile.json")
             subprocess.call(["oc", "get", "pods"])
 
-            snumber = snumber + 1
+            minpod = minpod + 1
 
     create_ebs_pod()
 
-
 if __name__ == '__main__':
     main()
+
 
