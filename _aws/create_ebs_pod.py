f7f2864757d1dedd357aafe5840a886271d115a9
Elvir Kuric <elvirkuric@gmail.com>
2016-03-20 08:47:36 +0100

    updated create_ebs_pod.py, added classes and some other changes
    Signed-off-by: Elvir Kuric <elvirkuric@gmail.com>

--- _aws/create_ebs_pod.py
+++ _aws/create_ebs_pod.py
@@ -1,4 +1,8 @@
-# program to create Amazon EC2 volumes
+#!/usr/bin/env python 
+
+# program to create OSE 3 pods with Amazon EC2 volumes as persistent storage 
+# License GPL2/3 
+
 __author__ = "elko"
 
 import argparse
@@ -6,12 +10,129 @@ import boto3
 import subprocess
 import os
 import json
-import time
+import time 
 
 
-def main():
+class CreateEbs():
 
-    # optional arguments
+    def __init__(self,volumesize,vtype,region,tagprefix,mountpoint,image,pvfile,pvcfile,podfile,minpod,maxpod):
+
+        self.volumesize = volumesize
+        self.vtype = vtype
+        self.region = region
+        self.tagprefix = tagprefix
+        self.mountpoint = mountpoint 
+        self.image = image
+        self.pvfile = pvfile
+        self.pvcfile = pvcfile
+        self.podfile = podfile
+        self.minpod = minpod
+        self.maxpod = maxpod
+        session = boto3.Session(region_name=region)
+        ec2 = boto3.resource("ec2")
+
+    # create EC2 volume to be used later for pv/pvc and pod 
+    # we create one EBS at time and tag it. Tags are only used for easier
+    # retreival when we want to delete EBSes
+
+    def ec2_volume(self,volumesize,vtype,region,tagprefix,mountpoint):
+
+        self.volumesize = 1
+        self.vtype = vtype  
+        self.region = "us-west-2b"
+        self.tagprefix = tagprefix
+        self.mountpoint = "/mnt/persistentvolume"
+        global tags
+        global volumeid 
+
+        session = boto3.Session(region_name=region)
+        ec2 = boto3.resource("ec2")
+        volume = ec2.create_volume(VolumeType=vtype,
+                                       AvailabilityZone=region,
+                                       Size=volumesize)
+
+        volumeid = volume.id
+
+        # volume created ... tag it for easier finding later - based on this tag
+        # eventually, write volume.id to file and not use tagging - to reduce load on Amazon EC2 API 
+        # more http://docs.aws.amazon.com/AWSEC2/latest/APIReference/query-api-troubleshooting.html#api-request-rate 
+        # adding time.sleep(10) - however this does not solve problem fully 
+        print ("Sleep 10 seconds before tagging volume")
+        time.sleep(10)
+
+        tags = ec2.create_tags(DryRun=False, Resources=[volume.id],
+                                   Tags=[
+                                       {'Key': tagprefix + volume.id,
+                                        'Value': tagprefix
+                                        },
+                                   ])
+    # create persistent volume 
+    def pvolume(self,pvfile):
+
+        self.pvfile = pvfile
+        try:
+            pvhandler = open(pvfile,"r")
+        except FileNotFoundError:
+            print ("Not possible to find file... check input parameters")
+
+        # file is opened, now, json load it 
+        pvjson = json.load(pvhandler)
+        
+        pvvanila = { "metadata": {"name": "pvolume" + str(minpod) },
+                     'spec': {'capacity': {'storage': volumesize },
+                              'persistentVolumeReclaimPolicy': 'Recycle', 'accessModes': ['ReadWriteOnce'],
+                              'awsElasticBlockStore': {'volumeid': volumeid , 'fsType': 'ext4'}}}
+        pvjson.update(pvvanila)
+        json.dump(pvjson, open("pvfile.json", "w+"),sort_keys=True, indent=4, separators=(',', ': '))
+        subprocess.call(["oc", "create", "-f", "pvfile.json"])
+        os.remove("pvfile.json")
+
+    # create persistent volume claim 
+    def pclaim(self,pvcfile):
+
+        self.pvcfile = pvcfile
+        try:
+            pvchandler = open(pvcfile,"r")
+        except FileNotFoundError:
+            print ("Not possible to find file ... check input parameters")
+
+
+        pvcjson = json.load(pvchandler)
+        pvcvanila = {'metadata': {"name":"pvclaim" + str(minpod)},
+                          'spec': {'accessModes': ['ReadWriteOnce'], 'resources': {'requests': {'storage': volumesize }}}}
+        pvcjson.update(pvcvanila)
+        json.dump(pvcjson, open("pvcfile.json", "w+"),sort_keys=True, indent=4, separators=(',', ': '))
+        subprocess.call(["oc", "create", "-f", "pvcfile.json" ])
+        os.remove("pvcfile.json")
+
+    # create pod 
+    def ppod(self,image,maxpod,minpod,podfile):
+
+        self.image = image 
+        self.maxpod = maxpod 
+        self.minpod = minpod 
+        self.podfile = podfile 
+
+        try:
+            podfile = open(self.podfile,"r")
+        except FileNotFoundError:
+            print ("Not possible to find file ... check input parameters")
+
+        podjson = json.load(podfile)
+        podvanila = {'metadata': {'name': "pod" + str(minpod)},
+                         'spec': {'containers': [{'image': image,
+                                                  'imagePullPolicy': 'IfNotPresent',
+                                                  'name': "pod" + str(minpod),
+                                                  'volumeMounts': [{'name': 'pvolume' + str(minpod),
+                                                  'mountPath': mountpoint }]}],
+                                                  'volumes': [{'persistentVolumeClaim': {'claimName': "pvclaim" + str(minpod)}, 'name': "pvolume" + str(minpod)}]}}
+        podjson.update(podvanila)
+        json.dump(podjson,  open("podfile.json", "w+"),sort_keys=True, indent=4, separators=(',', ': '))
+        subprocess.call(["oc", "create", "-f", "podfile.json"])
+        os.remove("podfile.json")
+
+if __name__ == "__main__":
+
     parser = argparse.ArgumentParser(description="Script to create OSE pods and attach one EBS volume per pod as persistent storage")
     parser.add_argument("--volumesize", help="size of EBS voluems - in GB ", default=5, type=int)
     parser.add_argument("--vtype", help="EBS volume type, default is gp2", default="gp2")
@@ -28,113 +149,30 @@ def main():
     parser.add_argument("--pvcfile", help="persistent volume claim definition json file - required parameter", required=True)
     parser.add_argument("--podfile", help="pod definition json file - required parameter", required=True)
 
-
     args = parser.parse_args()
 
     volumesize = args.volumesize
     vtype = args.vtype
     region = args.region
+    image = args.image 
     tagprefix = args.tagprefix
     mountpoint = args.mountpoint
-    image = args.image
+    minpod = args.minpod 
+    maxpod = args.maxpod 
     pvfile = args.pvfile
     pvcfile = args.pvcfile
     podfile = args.podfile
-    minpod = args.minpod
-    maxpod = args.maxpod
     action = args.action
-
-    session = boto3.Session(region_name=region)
-    ec2 = boto3.resource("ec2")
-
-    def create_ebs_pod():
-        """
-        create_ebs_pod() function will do
-        - create EBS volume
-        - tag EBS volume
-        - create PV/PVC
-        - crete pod and attach already created EBS volume to POD as persistent storage
-        """
-        global minpod
-        minpod = args.minpod
-
-        while minpod < maxpod:
-
-            volume = ec2.create_volume(VolumeType=vtype,
-                                       AvailabilityZone=region,
-                                       Size=volumesize)
-
-            # volume created ... tag it for easier finding later - based on this tag
-            tags = ec2.create_tags(DryRun=False, Resources=[volume.id],
-                                   Tags=[
-                                       {'Key': tagprefix + volume.id,
-                                        'Value': tagprefix
-                                        },
-                                   ])
-            # due to http://docs.aws.amazon.com/AWSEC2/latest/APIReference/query-api-troubleshooting.html#api-request-rate 
-            # adding time.sleep(15) - however this does not show issue fully, request limit rate can happen again, what is
-            # ihmo Amazon AWS issue 
-            print ("Sleeping 20 seconds ... while waiting read http://docs.aws.amazon.com/AWSEC2/latest/APIReference/query-api-troubleshooting.html#api-request-rate")
-            time.sleep(20)
-            # create pv 
-
-            try:
-                pvhandler = open(pvfile,"r")
-            except IOError:
-                    print ("Cannot open file persistent volume file, is it specified?")
-
-            pvdoc = json.load(pvhandler)
-            pvvanila = { "metadata": {"name": "pvolume" + str(minpod) },
-                       'spec': {'capacity': {'storage': volumesize },
-                        'persistentVolumeReclaimPolicy': 'Recycle', 'accessModes': ['ReadWriteOnce'],
-                        'awsElasticBlockStore': {'volumeID': volume.id , 'fsType': 'ext4'}}}
-
-            pvdoc.update(pvvanila)
-            json.dump(pvdoc, open("pvfile.json","w+"))
-            # run an create now pv
-            subprocess.call(["oc", "create", "-f", "pvfile.json"])
-            os.remove("pvfile.json")
-
-            # todo  - implement to check does PV/PVC/POD already exist on system
-            # create pvc
-            try:
-                pvchandler = open(pvcfile,"r")
-            except IOError:
-                print ("Cannot open persistent volume claim file, is it specified?")
-
-            pvcdoc = json.load(pvchandler)
-            pvcvanila = {'metadata': {"name":"pvclaim" + str(minpod)},
-                          'spec': {'accessModes': ['ReadWriteOnce'], 'resources': {'requests': {'storage': volumesize }}}}
-
-            pvcdoc.update(pvcvanila)
-            json.dump(pvcdoc, open("pvcfile.json", "w+"))
-            subprocess.call(["oc", "create", "-f", "pvcfile.json"])
-            os.remove("pvcfile.json")
-            # create pod
-
-            try:
-                podhandler = open(podfile, "r")
-            except IOError:
-                print ("Cannot open pod file, is it specified? Check options")
-
-            poddoc = json.load(podhandler)
-            podvanila = {'metadata': {'name': "pod" + str(minpod)},
-                         'spec': {'containers': [{'image': image,
-                                                  'imagePullPolicy': 'IfNotPresent',
-                                                  'name': "pod" + str(minpod),
-                                                  'volumeMounts': [{'name': 'pvolume' + str(minpod),
-                                                  'mountPath': mountpoint }]}],
-                                                  'volumes': [{'persistentVolumeClaim': {'claimName': "pvclaim" + str(minpod)}, 'name': "pvolume" + str(minpod)}]}}
-            poddoc.update(podvanila)
-            json.dump(poddoc, open("podfile.json", "w+"))
-            subprocess.call(["oc", "create", "-f", "podfile.json"])
-            os.remove("podfile.json")
-
-            minpod = minpod + 1
-
-    create_ebs_pod()
-
-if __name__ == '__main__':
-    main()
+    
+    create_ebs = CreateEbs(volumesize,vtype,region,tagprefix,mountpoint,image,pvfile,pvcfile,podfile,minpod,maxpod)
+    total_pods = maxpod - minpod 
+    while minpod < maxpod:
+        create_ebs.ec2_volume(volumesize,vtype,region,tagprefix,mountpoint)
+        create_ebs.pvolume(pvfile)
+        create_ebs.pclaim(pvcfile)
+        create_ebs.ppod(image,maxpod,minpod,podfile)
+        minpod = minpod + 1 
+    
+    print ( total_pods , "pods which use EBS as pessistent storage are created")
 
 
