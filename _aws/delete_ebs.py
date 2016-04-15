baf652b97c23d391212119439a142a9948a6218a
Elvir Kuric <elvirkuric@gmail.com>
2016-04-15 20:21:11 +0200

    small change to adapt on amazon api limitation ( backoff algorithm)
    Signed-off-by: Elvir Kuric <elvirkuric@gmail.com>

--- _aws/delete_ebs.py
+++ _aws/delete_ebs.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
+# Script to delete tagged EBS volumes
+# License GPLv3 http://www.gnu.org/licenses/quick-guide-gplv3.en.html 
+
 import boto3
 import argparse
+import botocore  
 
 def main():
     parser = argparse.ArgumentParser(description="Script to delete EBS volume - based on tag it has")
@@ -14,13 +18,18 @@ def main():
     def delete_ebs():
         global tagname
         tagname = args.tagname
-
-        volumestag = ec2.volumes.filter(Filters=[{'Name' : 'tag-value', 'Values':[tagname]}])
-        # for keys it would be ec2.volumes.filter(Filters=[{'Name' : 'tag-key', 'Values':[tagkey]}])
-        for volume in volumestag:
-            print ("Deleting volume:", volume.id)
-            volume.delete(DryRun=False)
-
+        while True:
+            try:
+                volumestag = ec2.volumes.filter(Filters=[{'Name' : 'tag-value', 'Values':[tagname]}])
+                # for keys it would be ec2.volumes.filter(Filters=[{'Name' : 'tag-key', 'Values':[tagkey]}])
+                for volume in volumestag:
+                    print ("Deleting volume:", volume.id)
+                    volume.delete(DryRun=False)
+            except botocore.exceptions.ClientError as err:
+                print("An exception occured", err.response["Error"]["Code"])
+                continue 
+            else:
+                print ("Volume deleted:")
     delete_ebs()
 
 
