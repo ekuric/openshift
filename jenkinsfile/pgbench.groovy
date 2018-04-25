10cbb5516a34043c1a71fa38551e4a6eb3516bf4
Elvir Kuric <elvirkuric@gmail.com>
2018-04-25 08:02:32 +0200

    changed label value
    Signed-off-by: Elvir Kuric <elvirkuric@gmail.com>

--- jenkinsfile/pgbench.groovy
+++ jenkinsfile/pgbench.groovy
@@ -2,7 +2,8 @@
 
 def pipeline_id = env.BUILD_ID
 println "Current pipeline job build id is '${pipeline_id}'"
-def node_label = 'CCI && ansible-2.3'
+//def node_label = 'CCI && ansible-2.3'
+def node_label = 'CCI && US' 
 def pgbench_test = PGBENCH_TEST.toString().toUpperCase()
 
 // run pgbench scale test
