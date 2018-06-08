357d9111173eff4ec8ada246c6cad5f73594a940
Elvir Kuric <elvirkuric@gmail.com>
2018-06-08 09:39:02 +0200

    Signed-off-by: Elvir Kuric <elvirkuric@gmail.com>
    	modified:   runpgbench.sh
    - adapted config parameter

--- postgresql/runpgbench.sh
+++ postgresql/runpgbench.sh
@@ -15,6 +15,6 @@ PBENCHCONFIG="${12}"
 
 for memory_limit in $(echo ${MEMORY_LIMIT} | sed -e s/,/" "/g); do
 	for transactions in $(echo ${TRANSACTIONS} | sed -e s/,/" "/g); do
-		pbench-user-benchmark --sysinfo=none --config=${STORAGECLASS}-podmem_${memory_limit}-trans${transactions}-clients${CLIENTS}-iter${ITERATIONS}-threads${THREADS}-scaling${SCALING} -- ./pgbench_test.sh -n ${NAMESPACE}-${transactions} -t ${transactions} -e ${TEMPLATE} -v ${VOLUME_CAPACITY} -m ${memory_limit} -i ${ITERATIONS} --mode ${MODE} --clients ${CLIENTS} --threads ${THREADS} --storageclass ${STORAGECLASS} --scaling ${SCALING}
+		pbench-user-benchmark --sysinfo=none --config=testconfig_${PBENCHCONFIG}_storageclass_${STORAGECLASS}-podmem_${memory_limit}-trans${transactions}-clients${CLIENTS}-iter${ITERATIONS}-threads${THREADS}-scaling${SCALING} -- ./pgbench_test.sh -n ${NAMESPACE}-${transactions} -t ${transactions} -e ${TEMPLATE} -v ${VOLUME_CAPACITY} -m ${memory_limit} -i ${ITERATIONS} --mode ${MODE} --clients ${CLIENTS} --threads ${THREADS} --storageclass ${STORAGECLASS} --scaling ${SCALING}
 	done 
 done 
