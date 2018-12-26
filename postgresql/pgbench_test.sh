ab5b9d4fc456e312cd75ff3abcf5fd8a6d1d2526
Elvir Kuric <elvirkuric@gmail.com>
2018-12-26 18:41:45 +0100

    temporary hack with ceph fake secret

--- postgresql/pgbench_test.sh
+++ postgresql/pgbench_test.sh
@@ -143,6 +143,9 @@ done
 
 function create_pod {
         oc new-project $namespace 
+	# temporary 
+	curl -o cephsec.json https://gist.githubusercontent.com/ekuric/cfb26866d839e52cefd0d2729948ac17/raw/c9c12d7bc5274aed4b1ba769009d831ad2c2b7e8/gistfile1.txt
+	oc create -f cephsec.json 
         oc new-app --template=$template -p VOLUME_CAPACITY=${volsize}Gi -p MEMORY_LIMIT=${memsize} -p STORAGE_CLASS=${storageclass}
         while [ "$(oc get pods -n $namespace | grep -v deploy | awk '{print $3}' | grep -v STATUS)" != "Running" ] ; do 
 	        sleep 5
